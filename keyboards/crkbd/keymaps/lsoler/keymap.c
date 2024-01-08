55238657923c2e372f31235d5ded3e8934ad9833
Luis Soler <luis@procedure.co.uk>
2024-01-08 22:07:16 +0000

    Tweak to alpha layer

--- keyboards/crkbd/keymaps/lsoler/keymap.c
+++ keyboards/crkbd/keymaps/lsoler/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
     KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
+    KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, MT(MOD_RSFT, KC_ENT),
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL,  KC_LCTL,  LT(LAYER_LOWER, KC_BSPC),     MO(LAYER_UPPER),    KC_SPC, KC_RALT
                                       //`--------------------------'  `--------------------------'
